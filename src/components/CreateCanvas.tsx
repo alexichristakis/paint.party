11eb1ab5ac1c8b1466bc6f319aefe70e4ebb0d1f
Alexi Christakis <alexi.christakis@gmail.com>
2020-03-27 23:26:50 -0700

    new create canvas button

--- src/components/CreateCanvas.tsx
+++ src/components/CreateCanvas.tsx
@@ -5,6 +5,7 @@ import {
   ActivityIndicator,
   Keyboard
 } from "react-native";
+import { useSelector } from "react-redux";
 
 import moment from "moment";
 import Animated from "react-native-reanimated";
@@ -14,6 +15,7 @@ import {
   bInterpolate
 } from "react-native-redash";
 
+import * as selectors from "@redux/selectors";
 import { NewCanvas } from "@redux/modules/canvas";
 import { ModalList, ModalListRef } from "./ModalList";
 import { Input, Slider, BackgroundColorPicker } from "./universal";
@@ -21,11 +23,10 @@ import { TextStyles, TextSizes, Colors } from "@lib";
 
 import { TouchableScale } from "./universal/TouchableScale";
 import Send from "@assets/svg/send.svg";
+import { useReduxAction } from "@hooks";
+import { CanvasActions } from "@redux/modules";
 
-export interface CreateCanvasProps {
-  loading: boolean;
-  onCreate: (canvas: NewCanvas) => void;
-}
+export interface CreateCanvasProps {}
 
 export type CreateCanvasRef = {
   open: () => void;
@@ -33,121 +34,122 @@ export type CreateCanvasRef = {
 };
 
 export const CreateCanvas = React.memo(
-  React.forwardRef<CreateCanvasRef, CreateCanvasProps>(
-    ({ loading, onCreate }, ref) => {
-      const textInputRef = useRef<TextInput>(null);
-      const modalRef = useRef<ModalListRef>(null);
+  React.forwardRef<CreateCanvasRef, CreateCanvasProps>(({}, ref) => {
+    const textInputRef = useRef<TextInput>(null);
+    const modalRef = useRef<ModalListRef>(null);
 
-      const [sliderValue] = useValues([0], []);
-      const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
-      const [expiry, setExpiry] = useState(moment().add(1, "day"));
-      const [name, setName] = useState("");
+    const [sliderValue] = useValues([0], []);
+    const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
+    const [expiry, setExpiry] = useState(moment().add(1, "day"));
+    const [name, setName] = useState("");
 
-      useImperativeHandle(ref, () => ({
-        open: () => {
-          modalRef.current?.open();
-          // setTimeout(() => textInputRef.current?.focus(), 300);
-        },
-        close: () => {
-          modalRef.current?.close();
-          setTimeout(() => textInputRef.current?.blur(), 100);
-        }
-      }));
+    const loading = useSelector(selectors.isCreatingCanvas);
+    const createCanvas = useReduxAction(CanvasActions.create);
 
-      const handleOnSnap = (index: number) => {
-        if (!index) {
-          setName("");
-          // textInputRef.current?.blur();
-          Keyboard.dismiss();
-        }
-      };
+    useImperativeHandle(ref, () => ({
+      open: () => {
+        modalRef.current?.open();
+        // setTimeout(() => textInputRef.current?.focus(), 300);
+      },
+      close: () => {
+        modalRef.current?.close();
+        setTimeout(() => textInputRef.current?.blur(), 100);
+      }
+    }));
 
-      const handleOnCompleteDrag = (val: number) => {
-        setExpiry(moment().add(val, "days"));
-      };
+    const handleOnSnap = (index: number) => {
+      if (!index) {
+        setName("");
+        // textInputRef.current?.blur();
+        Keyboard.dismiss();
+      }
+    };
 
-      const isValidCanvas = () => !!name.length;
+    const handleOnCompleteDrag = (val: number) => {
+      setExpiry(moment().add(val, "days"));
+    };
 
-      const handleOnPressCreateCanvas = () =>
-        onCreate({ name, expiresAt: expiry.unix(), backgroundColor });
+    const isValidCanvas = () => !!name.length;
 
-      const createCanvasButtonTransition = useSpringTransition(
-        isValidCanvas() && !loading
-      );
+    const handleOnPressCreateCanvas = () =>
+      createCanvas({ name, expiresAt: expiry.unix(), backgroundColor });
 
-      const range = [1, 5] as [number, number];
-      return (
-        <>
-          <ModalList
-            showHeader={false}
-            onSnap={handleOnSnap}
-            style={{ flex: 1, paddingHorizontal: 10 }}
-            ref={modalRef}
-          >
-            <Input
-              maxLength={30}
-              textInputRef={textInputRef}
-              autoCapitalize="none"
-              placeholder="canvas name"
-              size={TextSizes.title}
-              value={name}
-              onChangeText={setName}
-            />
-            <Animated.Text style={styles.text}>
-              expires {expiry.fromNow()}
-            </Animated.Text>
-            <Slider
-              style={{ marginBottom: 20 }}
-              onCompleteDrag={handleOnCompleteDrag}
-              value={sliderValue}
-              range={range}
-            />
-            <Animated.Text style={styles.text}>background color</Animated.Text>
-            <BackgroundColorPicker
-              selected={backgroundColor}
-              onChoose={setBackgroundColor}
-            />
+    const createCanvasButtonTransition = useSpringTransition(
+      isValidCanvas() && !loading
+    );
 
-            {loading ? (
+    const range = [1, 5] as [number, number];
+    return (
+      <>
+        <ModalList
+          showHeader={false}
+          onSnap={handleOnSnap}
+          style={{ flex: 1, paddingHorizontal: 10 }}
+          ref={modalRef}
+        >
+          <Input
+            maxLength={30}
+            textInputRef={textInputRef}
+            autoCapitalize="none"
+            placeholder="canvas name"
+            size={TextSizes.title}
+            value={name}
+            onChangeText={setName}
+          />
+          <Animated.Text style={styles.text}>
+            expires {expiry.fromNow()}
+          </Animated.Text>
+          <Slider
+            style={{ marginBottom: 20 }}
+            onCompleteDrag={handleOnCompleteDrag}
+            value={sliderValue}
+            range={range}
+          />
+          <Animated.Text style={styles.text}>background color</Animated.Text>
+          <BackgroundColorPicker
+            selected={backgroundColor}
+            onChoose={setBackgroundColor}
+          />
+
+          {loading ? (
+            <Animated.View
+              style={[
+                styles.sendButton,
+                {
+                  transform: [
+                    {
+                      scale: bInterpolate(createCanvasButtonTransition, 1, 0)
+                    }
+                  ],
+                  opacity: bInterpolate(createCanvasButtonTransition, 1, 0)
+                }
+              ]}
+            >
+              <ActivityIndicator
+                size="large"
+                style={{ width: 50, height: 50 }}
+              />
+            </Animated.View>
+          ) : (
+            <TouchableScale
+              onPress={handleOnPressCreateCanvas}
+              dependencies={[name, expiry.fromNow(), backgroundColor]}
+              style={styles.sendButton}
+            >
               <Animated.View
-                style={[
-                  styles.sendButton,
-                  {
-                    transform: [
-                      {
-                        scale: bInterpolate(createCanvasButtonTransition, 1, 0)
-                      }
-                    ],
-                    opacity: bInterpolate(createCanvasButtonTransition, 1, 0)
-                  }
-                ]}
+                style={{
+                  transform: [{ scale: createCanvasButtonTransition }],
+                  opacity: createCanvasButtonTransition
+                }}
               >
-                <ActivityIndicator
-                  size="large"
-                  style={{ width: 50, height: 50 }}
-                />
+                <Send height={50} width={50} />
               </Animated.View>
-            ) : (
-              <TouchableScale
-                onPress={handleOnPressCreateCanvas}
-                dependencies={[name, expiry.fromNow(), backgroundColor]}
-                style={styles.sendButton}
-              >
-                <Animated.View
-                  style={{
-                    transform: [{ scale: createCanvasButtonTransition }],
-                    opacity: createCanvasButtonTransition
-                  }}
-                >
-                  <Send height={50} width={50} />
-                </Animated.View>
-              </TouchableScale>
-            )}
-          </ModalList>
-        </>
-      );
-    }
-  )
+            </TouchableScale>
+          )}
+        </ModalList>
+      </>
+    );
+  })
 );
 
 const styles = StyleSheet.create({
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,25 +9,22 @@ import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "@redux/types";
 import * as selectors from "@redux/selectors";
 import { CanvasActions, AppActions } from "@redux/modules";
-import { CreateCanvas, CreateCanvasRef } from "@components/CreateCanvas";
 import { Canvases } from "@components/Canvases";
 
 import { StackParamList } from "../App";
 import { SB_HEIGHT, TextStyles } from "@lib";
-import Plus from "@assets/svg/plus.svg";
 import { useValues, onScroll } from "react-native-redash";
+import { NewCanvas } from "@components/NewCanvas";
 
 const connector = connect(
   (state: RootState) => ({
     activeCanvas: selectors.activeCanvas(state),
-    isCreatingCanvas: selectors.isCreatingCanvas(state),
     canvases: selectors.canvases(state)
   }),
   {
     logout: AppActions.logout,
     openCanvas: CanvasActions.open,
     unsubscribe: CanvasActions.close,
-    createCanvas: CanvasActions.create,
     fetchCanvases: CanvasActions.fetch
   }
 );
@@ -38,16 +35,13 @@ export interface HomeProps {
 }
 
 const Home: React.FC<HomeProps & HomeReduxProps> = ({
-  isCreatingCanvas,
   canvases,
   openCanvas,
   fetchCanvases,
   activeCanvas,
-  createCanvas,
   unsubscribe
 }) => {
   const [scrollY] = useValues([0], []);
-  const createCanvasRef = useRef<CreateCanvasRef>(null);
   // const [modalVisible, setModalVisible] = useState(false);
 
   //
@@ -66,32 +60,13 @@ const Home: React.FC<HomeProps & HomeReduxProps> = ({
         scrollEventThrottle={16}
         contentContainerStyle={styles.contentContainer}
       >
-        <TouchableOpacity
-          style={styles.header}
-          onPress={() => createCanvasRef.current?.open()}
-        >
-          {!values(canvases).length ? (
-            <View style={styles.headerContent}>
-              <Text style={styles.welcomeText}>
-                To start on a canvas, tap here or an invite link.
-              </Text>
-              <Plus width={40} height={40} />
-            </View>
-          ) : (
-            <Plus width={40} height={40} />
-          )}
-        </TouchableOpacity>
         <Canvases
           scrollY={scrollY}
           onPressCanvas={openCanvas}
           canvases={values(canvases)}
         />
       </Animated.ScrollView>
-      <CreateCanvas
-        ref={createCanvasRef}
-        loading={isCreatingCanvas}
-        onCreate={createCanvas}
-      />
+      <NewCanvas />
     </>
   );
 };
