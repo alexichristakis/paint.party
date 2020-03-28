d6b3451f10607a9161154d2dc70d9befb724864a
Alexi Christakis <alexi.christakis@gmail.com>
2020-03-28 00:12:52 -0700

    code push incompatible for now

--- android/app/src/main/java/com/draw/MainApplication.java
+++ android/app/src/main/java/com/draw/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,14 +28,22 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          packages.add(new RNNotificationsPackage(MainApplication.this));
+
           return packages;
         }
+
+        @Override
+        protected String getJSBundleFile() { return CodePush.getJSBundleFile(); }
 
         @Override
         protected String getJSMainModuleName() {
           return "index";
         }
       };
+
+
 
   @Override
   public ReactNativeHost getReactNativeHost() {
