f334da4ea642f837314a33bfd3765ce288a7ca09
Alexi Christakis <alexi.christakis@gmail.com>
2020-04-14 00:57:17 -0700

    made transition

--- src/components/Canvas/Visualization.tsx
+++ src/components/Canvas/Visualization.tsx
@@ -63,80 +63,14 @@ const Visualization: React.FC<
   VisualizationProps & VisualizationReduxProps
 > = React.memo(
   ({ pickerVisible, positionsVisible, selectCell, backgroundColor }) => {
-    const pinchRef = useRef<PinchGestureHandler>(null);
-    const panRef = useRef<PanGestureHandler>(null);
     const childRef = useRef<Animated.View>(null);
 
-    // const velocity = vec.createValue(0, 0);
-    // const drag = vec.create(0, 0);
+    const tap = useMemoOne(() => vec.createValue(0, 0), []);
 
-    const [pinchState, panState, tapState] = useValues<State>(
-      [UNDETERMINED, UNDETERMINED, UNDETERMINED],
-      []
-    );
+    const [tapState] = useValues<State>([UNDETERMINED], []);
 
-    const [translation, focal, velocity, tap, origin] = useMemoOne(
-      () => [
-        vec.createValue(0, 0),
-        vec.createValue(0, 0),
-        vec.createValue(0, 0),
-        vec.createValue(0, 0),
-        vec.createValue(0, 0),
-      ],
-      []
-    );
-
-    const [pinch] = useValues<number>([1], []);
-
-    // const [
-    //   translationX,
-    //   translationY,
-    //   focalX,
-    //   focalY,
-    //   velocityY,
-    //   velocityX,
-    //   tapX,
-    //   tapY,
-    //   pinch,
-    // ] = useValues<number>([0, 0, 0, 0, 0, 0, 0, 0, 1], []);
-
-    const [pinchHandler, panHandler, tapHandler] = useMemoOne(
-      () => [
-        onGestureEvent({
-          scale: pinch,
-          state: pinchState,
-          focalX: focal.x,
-          focalY: focal.y,
-        }),
-        onGestureEvent({
-          state: panState,
-          translationX: translation.x,
-          translationY: translation.y,
-          velocityX: velocity.x,
-          velocityY: velocity.y,
-        }),
-        onGestureEvent({ state: tapState, ...tap }),
-      ],
-      []
-    );
-
-    const deceleration = 0.992;
-    const [scale, translateY, translateX] = useMemoOne(
-      () => [
-        withScaleOffset(pinch, pinchState),
-        withDecay({
-          state: panState,
-          value: translation.y,
-          velocity: velocity.y,
-          deceleration,
-        }),
-        withDecay({
-          state: panState,
-          value: translation.x,
-          velocity: velocity.x,
-          deceleration,
-        }),
-      ],
+    const tapHandler = useMemoOne(
+      () => onGestureEvent({ state: tapState, ...tap }),
       []
     );
 
@@ -145,35 +79,8 @@ const Visualization: React.FC<
       []
     );
 
-    const adjustedFocal = vec.add(
-      {
-        x: -CANVAS_SIZE / 2,
-        y: CANVAS_SIZE / 2 - (SCREEN_HEIGHT - CANVAS_SIZE),
-      },
-      focal
-    );
-
-    // const trans = vec.createValue(0, 0);
-
-    const gestureBegan = or(eq(panState, ACTIVE), eq(pinchState, ACTIVE));
     useCode(
       () => [
-        cond(eq(pinchState, BEGAN), [
-          vec.set(origin, adjustedFocal),
-          debug("originX", origin.x),
-          debug("originY", origin.y),
-        ]),
-        onChange(
-          pinchState,
-          cond(eq(pinchState, END), [
-            //
-            vec.set(focal, trans),
-          ])
-        ),
-        onChange(
-          gestureBegan,
-          cond(and(gestureBegan, pickerVisible), set(pickerVisible, 0))
-        ),
         onPress(tapState, [
           call([tap.x, tap.y], handleOnPressCell),
           cond(not(pickerVisible), set(pickerVisible, 1)),
@@ -183,41 +90,9 @@ const Visualization: React.FC<
       []
     );
 
-    const trans = useMemoOne(() => {
-      const { x, y } = vec.invert(vec.sub(origin, adjustedFocal));
-
-      const start = and(neq(pinchState, BEGAN), neq(pinchState, UNDETERMINED));
-      return {
-        x: withOffset(cond(start, x, 0), pinchState),
-        y: withOffset(cond(start, y, 0), pinchState),
-      };
-    }, []);
-
-    useCode(
-      () => [
-        debug("x", trans.x),
-        debug("y", trans.y),
-        // debug("x", vec.invert(vec.sub(origin, adjustedFocal)).x),
-        // debug("y", vec.invert(vec.sub(origin, adjustedFocal)).y),
-      ],
-      []
-    );
-
-    const translationStyle = {}; // { transform: [{ translateX }, { translateY }] };
-    const scaleStyle = {
-      paddingTop: (SCREEN_HEIGHT - CANVAS_SIZE) / 2,
-      transform: [
-        ...translate(trans),
-        ...transformOrigin(origin, { scale }),
-        // ...translate(origin),
-        // { scale },
-        // ...translate(vec.multiply(origin, -1)),
-        // ...translate({ x: CANVAS_SIZE / 2, y: CANVAS_SIZE / 2 }),
-      ],
-    };
-
+    const onGestureBegan = cond(pickerVisible, set(pickerVisible, 0));
     return (
-      <ZoomPanHandler>
+      <ZoomPanHandler onGestureBegan={onGestureBegan}>
         <TapGestureHandler {...tapHandler}>
           <Animated.View ref={childRef}>
             <Grid backgroundColor={backgroundColor} />
@@ -226,36 +101,6 @@ const Visualization: React.FC<
           </Animated.View>
         </TapGestureHandler>
       </ZoomPanHandler>
-    );
-
-    return (
-      // <PanGestureHandler
-      //   avgTouches
-      //   ref={panRef}
-      //   minDist={10}
-      //   simultaneousHandlers={pinchRef}
-      //   {...panHandler}
-      // >
-      //   <Animated.View style={translationStyle}>
-      <PinchGestureHandler
-        ref={pinchRef}
-        simultaneousHandlers={panRef}
-        {...pinchHandler}
-      >
-        <Animated.View style={StyleSheet.absoluteFill}>
-          <Animated.View style={scaleStyle}>
-            {/* <TapGestureHandler {...tapHandler}>
-              <Animated.View ref={childRef}> */}
-            <Grid backgroundColor={backgroundColor} />
-            <PositionsOverlay visible={positionsVisible} />
-            <CellHighlight visible={pickerVisible} />
-            {/* </Animated.View> */}
-            {/* </TapGestureHandler> */}
-          </Animated.View>
-        </Animated.View>
-      </PinchGestureHandler>
-      //   </Animated.View>
-      // </PanGestureHandler>
     );
   },
   (p, n) => p.backgroundColor === n.backgroundColor
--- src/components/Canvas/ZoomPanHandler.tsx
+++ src/components/Canvas/ZoomPanHandler.tsx
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import { StyleSheet, View } from "react-native";
+import Animated, { Easing, useCode, onChange } from "react-native-reanimated";
+import { bin, useValues, onGestureEvent, translate } from "react-native-redash";
+import { useMemoOne } from "use-memo-one";
 import {
   PanGestureHandler,
   State,
   PinchGestureHandler,
 } from "react-native-gesture-handler";
 
-import Animated, { Easing } from "react-native-reanimated";
 import { CANVAS_SIZE } from "@lib";
 
 const {
@@ -41,13 +43,16 @@ const {
   event,
 } = Animated;
 
-function scaleDiff(value) {
+function scaleDiff(value: Animated.Adaptable<number>) {
   const tmp = new Value(1);
   const prev = new Value(1);
   return [set(tmp, divide(value, prev)), set(prev, value), tmp];
 }
 
-function dragDiff(value, updating) {
+function dragDiff(
+  value: Animated.Adaptable<number>,
+  updating: Animated.Adaptable<0 | 1>
+) {
   const tmp = new Value(0);
   const prev = new Value(0);
   return cond(
@@ -60,7 +65,7 @@ function dragDiff(value, updating) {
 // returns linear friction coeff. When `value` is 0 coeff is 1 (no friction), then
 // it grows linearly until it reaches `MAX_FRICTION` when `value` is equal
 // to `MAX_VALUE`
-function friction(value) {
+function friction(value: Animated.Adaptable<number>) {
   const MAX_FRICTION = 5;
   const MAX_VALUE = 100;
   return max(
@@ -69,16 +74,16 @@ function friction(value) {
   );
 }
 
-function speed(value) {
+function speed(value: Animated.Adaptable<number>) {
   const clock = new Clock();
   const dt = diff(clock);
   return cond(lessThan(dt, 1), 0, multiply(1000, divide(diff(value), dt)));
 }
 
 const MIN_SCALE = 1;
-const MAX_SCALE = 2;
+const MAX_SCALE = 4;
 
-function scaleRest(value) {
+function scaleRest(value: Animated.Adaptable<number>) {
   return cond(
     lessThan(value, MIN_SCALE),
     MIN_SCALE,
@@ -86,7 +91,11 @@ function scaleRest(value) {
   );
 }
 
-function scaleFriction(value, rest, delta) {
+function scaleFriction(
+  value: Animated.Adaptable<number>,
+  rest: Animated.Adaptable<number>,
+  delta: Animated.Adaptable<number>
+) {
   const MAX_FRICTION = 20;
   const MAX_VALUE = 0.5;
   const res = multiply(value, delta);
@@ -102,7 +111,12 @@ function scaleFriction(value, rest, delta) {
   );
 }
 
-function runTiming(clock, value, dest, startStopClock = true) {
+function runTiming(
+  clock: Animated.Clock,
+  value: Animated.Adaptable<number>,
+  dest: Animated.Adaptable<number>,
+  startStopClock = true
+) {
   const state = {
     finished: new Value(0),
     position: new Value(0),
@@ -123,15 +137,19 @@ function runTiming(clock, value, dest, startStopClock = true) {
       set(state.time, 0),
       set(state.position, value),
       set(config.toValue, dest),
-      startStopClock && startClock(clock),
+      cond(bin(startStopClock), startClock(clock)),
     ]),
     timing(clock, state, config),
-    cond(state.finished, startStopClock && stopClock(clock)),
+    cond(state.finished, cond(bin(startStopClock), stopClock(clock))),
     state.position,
   ];
 }
 
-function runDecay(clock, value, velocity) {
+function runDecay(
+  clock: Animated.Clock,
+  value: Animated.Adaptable<number>,
+  velocity: Animated.Adaptable<number>
+) {
   const state = {
     finished: new Value(0),
     velocity: new Value(0),
@@ -157,13 +175,13 @@ function runDecay(clock, value, velocity) {
 }
 
 function bouncyPinch(
-  value,
-  gesture,
-  gestureActive,
-  focalX,
-  displacementX,
-  focalY,
-  displacementY
+  value: Animated.Adaptable<number>,
+  gesture: Animated.Adaptable<number>,
+  gestureActive: Animated.Node<number>,
+  focalX: Animated.Adaptable<number>,
+  displacementX: Animated.Value<number>,
+  focalY: Animated.Adaptable<number>,
+  displacementY: Animated.Value<number>
 ) {
   const clock = new Clock();
 
@@ -209,12 +227,12 @@ function bouncyPinch(
 }
 
 function bouncy(
-  value,
-  gestureDiv,
-  gestureActive,
-  lowerBound,
-  upperBound,
-  friction
+  value: Animated.Adaptable<number>,
+  gestureDiv: Animated.Node<number>,
+  gestureActive: Animated.Node<number>,
+  lowerBound: Animated.Node<number>,
+  upperBound: Animated.Node<number>,
+  friction: (val: Animated.Node<number>) => Animated.Node<number>
 ) {
   const timingClock = new Clock();
   const decayClock = new Clock();
@@ -257,166 +275,149 @@ function bouncy(
 const WIDTH = CANVAS_SIZE;
 const HEIGHT = CANVAS_SIZE;
 
-export default class Viewer extends Component {
-  pinchRef = React.createRef();
-  panRef = React.createRef();
-  constructor(props) {
-    super(props);
-
-    // DECLARE TRANSX
-    const panTransX = new Value(0);
-    const panTransY = new Value(0);
-
-    // PINCH
-    const pinchScale = new Value(1);
-    const pinchFocalX = new Value(0);
-    const pinchFocalY = new Value(0);
-    const pinchState = new Value(-1);
-
-    this._onPinchEvent = event([
-      {
-        nativeEvent: {
-          state: pinchState,
-          scale: pinchScale,
-          focalX: pinchFocalX,
-          focalY: pinchFocalY,
-        },
-      },
-    ]);
-
-    // SCALE
-    const scale = new Value(1);
-    const pinchActive = eq(pinchState, State.ACTIVE);
-    this._focalDisplacementX = new Value(0);
-    const relativeFocalX = sub(
-      pinchFocalX,
-      add(panTransX, this._focalDisplacementX)
-    );
-    this._focalDisplacementY = new Value(0);
-    const relativeFocalY = sub(
-      pinchFocalY,
-      add(panTransY, this._focalDisplacementY)
-    );
-    this._scale = set(
-      scale,
-      bouncyPinch(
-        scale,
-        pinchScale,
-        pinchActive,
-        relativeFocalX,
-        this._focalDisplacementX,
-        relativeFocalY,
-        this._focalDisplacementY
-      )
-    );
-
-    // PAN
-    const dragX = new Value(0);
-    const dragY = new Value(0);
-    const panState = new Value(-1);
-    this._onPanEvent = event([
-      {
-        nativeEvent: {
-          translationX: dragX,
-          translationY: dragY,
-          state: panState,
-        },
-      },
-    ]);
-
-    const panActive = eq(panState, State.ACTIVE);
-    const panFriction = (value) => friction(value);
-
-    // X
-    const panUpX = cond(
-      lessThan(this._scale, 1),
-      0,
-      multiply(-1, this._focalDisplacementX)
-    );
-    const panLowX = add(panUpX, multiply(-WIDTH, add(max(1, this._scale), -1)));
-    this._panTransX = set(
-      panTransX,
-      bouncy(
-        panTransX,
-        dragDiff(dragX, panActive),
-        or(panActive, pinchActive),
-        panLowX,
-        panUpX,
-        panFriction
-      )
-    );
-
-    // Y
-    const panUpY = cond(
-      lessThan(this._scale, 1),
-      0,
-      multiply(-1, this._focalDisplacementY)
-    );
-    const panLowY = add(
-      panUpY,
-      multiply(-HEIGHT, add(max(1, this._scale), -1))
-    );
-    this._panTransY = set(
-      panTransY,
-      bouncy(
-        panTransY,
-        dragDiff(dragY, panActive),
-        or(panActive, pinchActive),
-        panLowY,
-        panUpY,
-        panFriction
-      )
-    );
-  }
-  render() {
-    // The below two animated values makes it so that scale appears to be done
-    // from the top left corner of the image view instead of its center. This
-    // is required for the "scale focal point" math to work correctly
-    const scaleTopLeftFixX = divide(multiply(WIDTH, add(this._scale, -1)), 2);
-    const scaleTopLeftFixY = divide(multiply(HEIGHT, add(this._scale, -1)), 2);
-    return (
-      <View style={styles.wrapper}>
-        <PinchGestureHandler
-          ref={this.pinchRef}
-          simultaneousHandlers={this.panRef}
-          onGestureEvent={this._onPinchEvent}
-          onHandlerStateChange={this._onPinchEvent}
-        >
-          <Animated.View>
-            <PanGestureHandler
-              ref={this.panRef}
-              minDist={10}
-              avgTouches
-              simultaneousHandlers={this.pinchRef}
-              onGestureEvent={this._onPanEvent}
-              onHandlerStateChange={this._onPanEvent}
-            >
-              <Animated.View
-                style={[
-                  {
-                    width: CANVAS_SIZE,
-                    height: CANVAS_SIZE,
-                    transform: [
-                      { translateX: this._panTransX },
-                      { translateY: this._panTransY },
-                      { translateX: this._focalDisplacementX },
-                      { translateY: this._focalDisplacementY },
-                      { translateX: scaleTopLeftFixX },
-                      { translateY: scaleTopLeftFixY },
-                      { scale: this._scale },
-                    ],
-                  },
-                ]}
-              >
-                {this.props.children}
-              </Animated.View>
-            </PanGestureHandler>
-          </Animated.View>
-        </PinchGestureHandler>
-      </View>
-    );
-  }
+export interface ZoomPanHandlerProps {
+  onGestureBegan: Animated.Adaptable<number>;
 }
+
+const ZoomPanHandler: React.FC<ZoomPanHandlerProps> = ({
+  children,
+  onGestureBegan,
+}) => {
+  const pinchRef = useRef<PinchGestureHandler>(null);
+  const panRef = useRef<PanGestureHandler>(null);
+
+  const [
+    panTransX,
+    panTransY,
+    pinchScale,
+    pinchFocalX,
+    pinchFocalY,
+    focalDisplacementX,
+    focalDisplacementY,
+    scale,
+    dragX,
+    dragY,
+  ] = useValues<number>([0, 0, 1, 0, 0, 0, 0, 1, 0, 0], []);
+
+  const [pinchState, panState] = useValues(
+    [State.UNDETERMINED, State.UNDETERMINED],
+    []
+  );
+
+  const pinchActive = eq(pinchState, State.ACTIVE);
+  const pinchHandler = onGestureEvent({
+    state: pinchState,
+    scale: pinchScale,
+    focalX: pinchFocalX,
+    focalY: pinchFocalY,
+  });
+
+  const panHandler = onGestureEvent({
+    state: panState,
+    translationX: dragX,
+    translationY: dragY,
+  });
+
+  const relativeFocalX = sub(pinchFocalX, add(panTransX, focalDisplacementX));
+  const relativeFocalY = sub(pinchFocalY, add(panTransY, focalDisplacementY));
+
+  const _scale = set(
+    scale,
+    bouncyPinch(
+      scale,
+      pinchScale,
+      pinchActive,
+      relativeFocalX,
+      focalDisplacementX,
+      relativeFocalY,
+      focalDisplacementY
+    )
+  );
+
+  const panActive = eq(panState, State.ACTIVE);
+
+  // X
+  const panUpX = cond(lessThan(_scale, 1), 0, multiply(-1, focalDisplacementX));
+  const panLowX = add(panUpX, multiply(-WIDTH, add(max(1, _scale), -1)));
+
+  // Y
+  const panUpY = cond(lessThan(_scale, 1), 0, multiply(-1, focalDisplacementY));
+  const panLowY = add(panUpY, multiply(-HEIGHT, add(max(1, _scale), -1)));
+
+  const trans = useMemoOne(
+    () => ({
+      x: set(
+        panTransX,
+        bouncy(
+          panTransX,
+          dragDiff(dragX, panActive),
+          or(panActive, pinchActive),
+          panLowX,
+          panUpX,
+          friction
+        )
+      ),
+      y: set(
+        panTransY,
+        bouncy(
+          panTransY,
+          dragDiff(dragY, panActive),
+          or(panActive, pinchActive),
+          panLowY,
+          panUpY,
+          friction
+        )
+      ),
+    }),
+    []
+  );
+
+  const gestureBegan = or(panActive, pinchActive);
+  useCode(() => [onChange(gestureBegan, onGestureBegan)], []);
+
+  const scaleTopLeftFixX = divide(multiply(WIDTH, add(_scale, -1)), 2);
+  const scaleTopLeftFixY = divide(multiply(HEIGHT, add(_scale, -1)), 2);
+  return (
+    <View style={styles.wrapper}>
+      <PinchGestureHandler
+        ref={pinchRef}
+        simultaneousHandlers={panRef}
+        {...pinchHandler}
+      >
+        <Animated.View>
+          <PanGestureHandler
+            ref={panRef}
+            minDist={10}
+            avgTouches
+            simultaneousHandlers={pinchRef}
+            {...panHandler}
+          >
+            <Animated.View
+              style={[
+                {
+                  width: CANVAS_SIZE,
+                  height: CANVAS_SIZE,
+                  transform: [
+                    ...translate(trans),
+                    { translateX: focalDisplacementX },
+                    { translateY: focalDisplacementY },
+                    { translateX: scaleTopLeftFixX },
+                    { translateY: scaleTopLeftFixY },
+                    { scale: _scale },
+                  ],
+                },
+              ]}
+            >
+              {children}
+            </Animated.View>
+          </PanGestureHandler>
+        </Animated.View>
+      </PinchGestureHandler>
+    </View>
+  );
+};
+
+export default ZoomPanHandler;
 
 const styles = StyleSheet.create({
   wrapper: {
